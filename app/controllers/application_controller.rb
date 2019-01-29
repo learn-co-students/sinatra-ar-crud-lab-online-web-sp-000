7979f5134396314c38d49665d8415180318e57dd
Aaron Berns <anberns@gmail.com>
2019-01-29 14:18:09 +0000

    create post route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require_relative '../.models/article.rb'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +9,12 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  post '/articles' do 
+    article = Article.new
+    article.create(title: params[:title], content: params[:content])
+    
+  
+  get '/articles/new' do
+    erb :new
   end
 end
