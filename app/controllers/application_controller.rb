4771fcfd08f0623d86997b84ed6eb83cd421d4ca
Aaron Berns <anberns@gmail.com>
2019-01-29 15:40:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,12 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
   
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+  
   get "/articles/:id" do
     @article = Article.find(params[:id])
     erb :show
@@ -31,4 +37,10 @@ class ApplicationController < Sinatra::Base
   get '/articles/:id/edit' do 
     @article = Article.find(params[:id])
     erb :edit
+  end
+  
+  delete "/articles/:id" do 
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
 end
