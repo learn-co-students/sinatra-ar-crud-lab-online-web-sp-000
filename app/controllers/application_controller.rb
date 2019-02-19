dfa531fc344c6b9230202bcea00b890e419ad8dd
Jessica Gladstone <jcg321@comcast.net>
2019-02-19 00:38:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +7,46 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+  
+   get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect '/articles/:id'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+      @article = Article.find(params[:id])
+      @article.title = params[:title]
+      @article.content = params[:content]
+      @article.save
+      erb :show
+    end
+
+  delete '/articles/:id/delete' do
+      @article = Article.find(params[:id])
+      @article.destroy
+      erb :show
+    end
 end
