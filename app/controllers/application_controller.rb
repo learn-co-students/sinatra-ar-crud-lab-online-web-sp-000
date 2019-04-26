ef038762a55b539fd9e1c6a9b18d1d0e75b388d4
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-26 01:00:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,19 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :'articles/new'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :'articles/show'
+  end
+
+  post '/articles' do
+    Article.create(params)
+    redirect to "/articles/#{Article.last.id}"
+  end
+
 end
