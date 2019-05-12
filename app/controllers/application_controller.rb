652433ed8b0ec42a834b168460ff113d3227bcdc
Tommy Curley <thomas.curley93@gmail.com>
2019-05-12 14:59:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,39 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles/:id' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  patch '/articles/:id' do
+    @article = Article.update(params[:id], title: params[:title], content: params[:content])
+    erb :show
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    @articles = Article.all
+    erb :index
+  end
 end
