216f7c10e9f124ab2f6a11d87297b1d5505d440b
ADIL NAIMI <adilnm23@hotmail.com>
2019-06-04 18:03:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,35 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+
+    erb :new
+  end
+  get '/articles/:id' do
+    @article=Article.all[params[:id].to_i-1]
+
+    erb :show
+end
+
+  post '/articles' do
+    @article=Article.create(params)
+
+    redirect :"/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+
+    erb :index
+  end
+
+  get '/articles' do
+    @articles=Article.all
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+
+    erb :edit
+  end
 end
