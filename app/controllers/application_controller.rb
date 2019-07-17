fa06e878e87ffefd6815e01538c5c74a0c3e065a
Alan Krajina <alankrajina88@gmail.com>
2019-07-17 14:10:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,18 +13,19 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-    Article.create(params[:student])
+    Article.create(params)
+  
+  redirect '/articles'
 
   end
   
   get '/articles' do
     @articles = Article.all
-    
     erb :index
   end
   
   get '/articles/:id' do
-    @article = Article.find(params[:id])
+    @article = Article.find(params['id'])
   
   erb :show
 
