a054cc5c095a999105c8a2de50aff8bb439ddc96
Shawn <shawntennity@gmail.com>
2019-08-05 17:31:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,56 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  # Show all articles
+  get '/articles' do  
+    @articles = Article.all
+
+    erb :"index"
+  end   
+
+  # Page with form to create new articles
+  get '/articles/new' do
+
+    erb :"new"
+  end
+
+  # Post request to create new article and redirect to article page
+  post '/articles' do
+    article = Article.create(params[:article])
+    
+    redirect to "articles/#{article.id}"
+  end
+  
+  # Display edit article page and form
+  get '/articles/:id/edit' do  
+    @article = Article.find(params[:id])    
+    # binding.pry
+    erb :"edit"
+  end
+  
+  # Update article
+  patch '/articles/:id' do 
+    # binding.pry
+    article = Article.find(params[:id])    
+    article.update(params[:article])
+    
+    redirect to "articles/#{article.id}"
+  end   
+
+  # Delete article
+  delete '/articles/:id' do 
+    # binding.pry
+    article = Article.find(params[:id])
+    article.destroy
+
+    redirect to "/articles"
+  end   
+
+  # Display article page
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :"show"
+  end
 end
