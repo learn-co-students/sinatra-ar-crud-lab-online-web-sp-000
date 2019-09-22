6acb617e23b0057551e78c10044968aa6276caf8
Frederic Silber <freddy.silber@cloudcoach.com>
2019-09-22 22:48:22 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < Sinatra::Base
   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
+    # @article.update
     redirect to "/articles/#{ @article.id }"
   end
 
