4b6d66d14118306f75db92acd81f565a99d15a57
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-09-27 22:03:40 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,3 @@
 class Article < ActiveRecord::Base
 
-    attr_accessor :title, :content
-
-
-    def initialize(args)
-        @title = args[:title]
-        @content = args[:content]
-    end
-
 end
