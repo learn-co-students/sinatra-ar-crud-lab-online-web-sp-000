b8e32456d4296804d75f2b2acde9c47317280af2
Karen V <karenvo@usc.edu>
2019-11-07 11:22:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require_relative '../models/article.rb'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +9,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
+
+    redirect "/articles/#{id}"
+  end
+
+  post '/articles' do
+    na = Article.create(params)
+    id_na = na.id
+    redirect "/articles/#{id_na}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    erb :delete
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params["id"])
+    @article.destroy
+
+    redirect '/articles'
+  end
+
 end
