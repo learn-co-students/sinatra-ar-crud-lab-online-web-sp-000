6997587fe9f98a50c344acf515fcd1e419cb5191
Michael Carroll <carrollm2@gmail.com>
2020-03-09 03:50:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,36 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+
+  get '/articles/new' do
+
+    erb :'articles/new'
   end
+
+  post '/articles' do
+
+    @created_article = Article.create(title: params[:title], content: params[:content])
+    Article.all << @created_article
+
+    erb :'articles/show'
+  end
+
+  get '/articles' do
+
+    @articles = Article.all
+    erb :'articles/index'
+  end
+
+  get '/articles/:id' do
+
+    @article = Article.find(params[:id])
+    erb :'articles/show'
+  end
+
+  get '/articles/:id/edit' do
+
+    erb :'articles/show'
+  end
+
+
 end
