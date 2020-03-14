8121e65179924d2849e69684a2ff118edb7d3b80
imjoshellis <joshe523@gmail.com>
2020-03-14 09:27:49 -0500

    retrieve old-code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,30 @@
-
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+    redirect to "/articles"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  post "/articles" do
+    redirect to "/show"
+  end
+
+  get "/show" do
+    @article
+    erb :show
   end
 end
