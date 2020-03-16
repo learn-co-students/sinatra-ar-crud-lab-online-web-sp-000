38af3d8a0bf2415a09f8ceeca2aa980caaea9aea
CoderJay06 <jaycru06@yahoo.com>
2020-03-16 03:36:27 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-  end
+  # get '/' do
+  # end
+
+  get '/articles/new' do 
+    #binding.pry 
+   
+    erb :new
+  end 
+
+  post '/articles' do 
+
+    @article = Article.create(title: params[:title], content: params[:title])
+
+    erb :show 
+  end 
 end
