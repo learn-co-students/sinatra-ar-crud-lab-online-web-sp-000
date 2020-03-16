74df0f3080bc04788e4468874d0327776fd1e485
CoderJay06 <jaycru06@yahoo.com>
2020-03-16 16:50:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do 
+  end 
+  # uses ar to grab all articles and stores them in @articles
   get '/articles' do
+    
     @articles = Article.all
-
+    
+    erb :index
   end
-
+  # renders the new.erb file 
   get '/articles/new' do 
     #binding.pry 
-
+    
     erb :new
   end 
-
+  # Creates a new article and redirects to /articles/:id'
   post '/articles' do 
     #@article = Article.create(title: params[:title], content: params[:content])
     @article = Article.new
@@ -28,12 +33,29 @@ class ApplicationController < Sinatra::Base
 
     redirect "/articles/#{@article.id}"
   end
-
+  # uses ar to grab the article with id and renders the show.erb
   get '/articles/:id' do 
     @article = Article.find(params[:id])
-
+    @article.save
     erb :show
   end 
-  
+  # renders edit.erb
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+
+    erb :edit 
+  end
+  # updates article, redirects to /articles/:id
+  patch '/articles/:id' do 
+    #binding.pry
+    @article = Article.find(params[:id])
+    # @article.title = params[:title]
+    # @article.content = params[:content]
+    # @article.save
+    @article.update 
+    @article.save 
+
+    redirect "/articles/#{@article.id}"
+  end 
 
 end
