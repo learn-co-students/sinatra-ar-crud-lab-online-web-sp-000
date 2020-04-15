6ace1b4aabdaa3ea88758afcd056e2c65a87b322
ksummerill <kaitlynsummerill@gmail.com>
2020-04-14 20:10:14 -0700

    AR CRUD lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,50 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  # The controller action should use the Create CRUD action to create the article and save it to the database.
+  get '/articles/new' do
+    @article = Article.new(params)
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    # binding.pry
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    # binding.pry
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect "/articles"
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  # 
+  # attr_reader :title, :content
+  #
+  # @@all = []
+  #
+  # def initialize(params)
+  #   @title = params[:title]
+  #   @content = params[:content]
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+
 end
