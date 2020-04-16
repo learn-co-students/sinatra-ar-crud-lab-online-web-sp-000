2550658fac41dfbb1990d9fa984818a278034294
nathan-stamand <nathansaint810@gmail.com>
2020-04-16 11:58:02 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +10,45 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    erb :new 
+  end
+
+  post '/articles' do 
+    @title = params[:title]
+    @content = params[:content]
+    Article.create(title: @title, content: @content)
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end 
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end 
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  delete '/articles/:id' do 
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    @articles = Article.all
+    redirect '/articles'
+  end 
+
 end
