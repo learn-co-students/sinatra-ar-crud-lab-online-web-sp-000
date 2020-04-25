631044f8a761ea99ee0d11bb254e738c8efeee3a
dkw5011 <dkw5011@gmail.com>
2020-04-25 19:47:41 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,10 +27,21 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
 
   
-
-  
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
 
 end
