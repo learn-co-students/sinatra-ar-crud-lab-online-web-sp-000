26b166432bf1df49367b9a7dcb844271a691ae8b
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-29 17:51:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Hello, World!"
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    new_article = Article.create(params)
+
+    redirect "/articles/#{new_article.id}"
+  end
+
+  get '/articles/:id' do
+    @new_article = Article.find(params[:id].to_i)
+
+    erb :show
   end
 end
