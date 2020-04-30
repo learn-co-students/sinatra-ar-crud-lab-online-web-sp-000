774e3017c0cd72dfdc1bf446be1a2bfc8ef25670
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-30 06:08:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +9,40 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    erb :index
   end
+
+
+  get '/articles/new' do
+    @title = params[:title]
+    @content = params[:content]
+    erb :new
+  end
+
+
+  get '/show' do
+    erb :show
+  end
+
+
+  get '/articles' do
+    # binding.pry
+    @articles = Article.all
+    erb :index
+  end
+
+
+  get '/articles/:id' do
+    # binding.pry
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+get '/articles/:id/edit' do
+  erb :edit
+end
+
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,24 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_reader :title, :content
+  @@all = []
+
+
+  def initialize(article)
+      # binding.pry
+  @title = article[:title]
+  @content = article[:content]
+
+  save
+  end
+
+  def self.all
+  @@all
+  end
+
+  def save
+    @@all << self
+  end
+
 end
