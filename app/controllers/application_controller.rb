9c92b94d35f8b8021583b91f3c1236c82572ba40
Chad Lengefeld <chadlengefeld@gmail.com>
2020-05-05 03:57:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,12 +26,7 @@ post '/articles' do
 
   end
 
-  # get '/show' do
-  #   erb :'/views/show'
-  # end
-
   get '/articles' do
-    # binding.pry
       @articles = Article.all
       @title = params[:title]
       @content = params[:content]
@@ -40,31 +35,25 @@ post '/articles' do
 
 
   get '/articles/:id' do
-  # binding.pry
     @article = Article.find(params[:id])
     erb :show
   end
 
 get '/articles/:id/edit' do
     @article = Article.find(params[:id])
-  # binding.pry
   erb :edit
 end
 
 patch '/articles/:id' do
-    # binding.pry
-  @article = Article.find(params[:id])
-  @article.title = params[:title]
-  @article.content = params[:content]
-  # Article.save
-   @article.save
-  redirect to "/articles/:id"
-
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+  redirect to "/articles/#{@article.id}"
   erb :show
 end
 
   delete '/articles/:id' do
-    Article.clear
+    @article = Article.find(params[:id])
+    @article.destroy
     erb :show
   end
 
