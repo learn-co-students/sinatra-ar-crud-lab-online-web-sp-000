fa7dfeb252e1c8c182909d2bdb72b6393cdf50ad
zchesny <z.chesny@gmail.com>
2020-05-11 17:17:24 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,53 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  # create
+  get '/articles/new' do
+    erb :new 
+  end 
+
+  # create
+  post '/articles' do 
+    article = Article.create(params)
+    redirect to "/articles/#{article.id}"
+  end 
+
+  # read
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  # read
+  get '/articles' do 
+    @articles = Article.all
+
+    erb :index 
+    #  <li><a href=<%="localhost:9393//articles/#{article.id}"%>><%= article.title %></a></li>
+  end 
+
+  # update 
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  # update
+  patch '/articles/:id' do 
+    article = Article.find(params[:id])
+    params.delete("_method")
+    article.update(params)
+    redirect to "/articles/#{params[:id]}"
+  end 
+
+  # delete 
+  delete '/articles/:id' do 
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
+
 end
