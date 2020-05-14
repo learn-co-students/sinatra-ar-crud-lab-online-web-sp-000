f76563c0ef050424d4b2992bfc2f4e0777b13d39
marcon29 <online@markstabler.com>
2020-05-14 16:00:42 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # create routes ###############################
+  get '/articles/new' do
+    erb :new
   end
+  
+  post '/articles' do
+    article = Article.new(params)
+    article.save  
+    redirect "/articles/#{article.id}"
+  end
+
+
+  # read routes ###############################
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do    
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # update routes ###############################
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])    
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    # article.update(params[:article])
+
+    params[:article].each do |attr, value|      
+      if value != article.send("#{attr}") && value != ""
+        article.send(("#{attr}="), value)
+      end
+    end
+    article.save
+
+    redirect "/articles/#{article.id}"
+  end
+  
+  # delete routes ###############################
+  delete '/articles/:id' do  
+    article = Article.find(params[:id])
+    article.destroy
+    redirect "/articles"
+  end
+ 
 end
