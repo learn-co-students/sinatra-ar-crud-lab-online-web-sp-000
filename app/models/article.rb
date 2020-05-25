8ec9e246f183d9f6084a59d763f935b9e684d90c
dayofgray <graysonpberger@gmail.com>
2020-05-25 16:45:27 -0400

    Revert "Done."
    This reverts commit 88759f38929557f6355b772359e68a2616644bd9.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,22 +1,4 @@
+#Placeholder for a model
+
 class Article < ActiveRecord::Base
-
-    # attr_accessor :title, :content, :id
-
-    # @@articles = []
-
-    # def initialize(title, content)
-    #     @title = title
-    #     @content = content
-    #     @@articles << self
-    # end
-
-    # # def initialize(args)
-    # #     @title = args[:title]
-    # #     @content = args[:content]
-    # #     @@articles << self
-    # # end
-
-    # def self.all
-    #     @@articles
-    # end
 end
