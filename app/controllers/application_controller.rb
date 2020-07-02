dcafe10763a5fa0f6fd2f5c499cd27658f4ea02d
clincoln12 <chris.lincoln12@gmail.com>
2020-07-02 10:53:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  post '/articles' do
+    article = Article.new(params)
+    article.save
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  delete '/articles/:id' do
+    article = Article.find(params[:id])
+    article.destroy
+    redirect "/articles/#{Article.last.id}"
   end
 end
