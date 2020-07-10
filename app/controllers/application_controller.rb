cc1b35400cdd6973034fd02cb324a4fcf3283905
AustinRhoads <austinrrhoads@ymail.com>
2020-07-10 03:04:09 -0500

    all tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+    
+  end
+
+
+
+  get '/articles/new' do
+     erb :new
+    end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  post '/articles' do 
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect '/articles/' + @article.id.to_s
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article.save
+    redirect '/articles/' + @article.id.to_s
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.delete
+  end
+
 end
