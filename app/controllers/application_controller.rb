e92aa415f05754d5909a8b5d8780429e528a3b71
Snowing222 <snowing222@gmail.com>
2020-08-20 06:32:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,40 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
-
+  
+  #index (read all)
   get '/articles' do 
     @articles = Article.all
     erb :index
   end
-
+  
+  #new (get user data to create/post)
   get '/articles/new' do
     erb :new
   end
-
+  
+  #show (read #)
   get '/articles/:id' do
     @article=Article.find(params[:id])
     erb :show
   end
 
-
+  #create (create w/user input)
   post '/articles' do
     @article=Article.create(title:params[:title], content:params[:content])
     redirect "/articles/#{@article.id}"
   end
 
+  #edit (find # and make change)
   get '/articles/:id/edit' do
     @article=Article.find(params[:id])
     erb :edit
   end
-
+  
+  #update (use change to update exsiting instance)
   patch '/articles/:id' do
     @article=Article.find(params[:id])
     @article.update(title:params[:title], content:params[:content])
-    
-    erb :show
+    redirect to "/articles/#{@article.id}"
   end
-
+  
+  #distroy
   delete '/articles/:id' do
     @article=Article.find(params[:id])
     @article.destroy
