288267d37e1efc7f0255088e363a15eeefc4ffd4
milos381 <milos_popovic@outlook.com>
2020-09-25 17:16:32 -0400

    Done.

--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -7,3 +7,5 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     end
   end
 end
+#this is where we create articles table withh two columns.
+#we can also use a command rake db:create_migration NAME=articles and later fill it out
