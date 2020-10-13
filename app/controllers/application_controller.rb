7f8c619d157ee1503f4638f4cead91735973293d
Cody Alvarez <cody_alvarez19@outlook.com>
2020-10-13 14:27:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,7 @@ class ApplicationController < Sinatra::Base
     redirect "/articles/#{@article.id}"
   end
 
-  delete 'articles/:id' do
+  delete '/articles/:id' do
     @article = Article.find(params[:id])
     @article.destroy
 
