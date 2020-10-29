4a0069d2144104c9414590d33bc33a08978c07f8
tsbrun <tsbrun@icloud.com>
2020-10-29 16:48:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do 
+    @article = Article.new(params[:article])
+    @article.save 
+
+    redirect to "/articles/#{@article.id}" 
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id].to_i)
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id].to_i)
+    @article.update(params[:article])
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do 
+    @article = Article.find(params[:id].to_i) 
+    @article.destroy
+
+    redirect to "/articles"
   end
 end
+
+# remember to convert params[:id] to integer
