585f7f8edc527933b272548b3d5bca8816de78be
marquesavery <averymarques4@gmail.com>
2020-11-25 16:41:01 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles' do
-    
+    @articles = Article.all
+    erb :index
   end
 
+  get '/articles/new' do
+  
+    erb :new
+  end
+
+  post '/articles' do
+    
+    @article = Article.create(params)
+
+    redirect '/articles/' + @article.id.to_s
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id].to_i)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id].to_i)
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id].to_i)
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by(id: params[:id].to_i)
+    @article.destroy
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,18 +1,6 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-    attr_accessor :title, :content
 
-    @@all = []
-
-    def initialize(args)
-        @title = args[:title]
-        @content = args[:content]
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-
+    
 end
