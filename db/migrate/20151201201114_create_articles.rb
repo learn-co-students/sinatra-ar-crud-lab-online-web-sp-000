53baa8da39866bf0c9d28a5ece864c3cbdcb8cba
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-25 08:24:35 +0000

    Done.

--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -1,9 +1,58 @@
-class CreateArticles < ActiveRecord::Migration[5.1]
-  def change
-    create_table :articles do |t|
-      t.string :title
-      t.string :content
+require_relative '../../config/environment'
 
-    end
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
   end
+
+  get '/' do
+    redirect to "/articles"
+  end
+
+  # index
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  # new
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
