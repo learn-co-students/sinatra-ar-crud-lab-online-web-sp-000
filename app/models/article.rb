f0d80572b2b05f0f658d2f507e18ee67ca329bdc
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-02-04 12:23:19 -0600

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    attr_reader :title, :content
+
+    @@all = []
+
+    def initialize(params)
+        @title = params[:title]
+        @content = params[:content]
+    end
+
+    def self.all
+        @@all
+    end
 end
