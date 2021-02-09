4a5796385c519db476ea1d722c18116fc0fb4ba0
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-02-09 13:47:33 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@
 require_relative '../../config/environment'
 require 'pry'
 
+# the order in which you define your routes in a controller matters
+# So, if we define the get '/articles/:id' route before the 
+# get '/articles/new' route, Sinatra would feed all requests for /articles/new to the /articles/:id route
+# and we should see an error telling us that our app is unable to find an Article instance with an id of "new".
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -16,29 +21,29 @@ class ApplicationController < Sinatra::Base
     redirect to '/articles'
   end
 
-  get '/articles' do
+  get '/articles' do # this action should use Active Record to grab all of the articles and store them in an instance variable.
     @articles = Article.all
     erb :index
   end
 
-  get '/articles/new' do
+  get '/articles/new' do # this action should use Active Record to create a new instance of an Article.
     @article = Article.new
     erb :new
   end
 
-  get '/articles/:id' do
+  get '/articles/:id' do # this action should use Active Record to grab the article with the id that is in the params and set it equal to an instance variable.
     @article = Article.find(params[:id])
     erb :show
   end
 
-  post '/articles' do
+  post '/articles' do # creates a new article from the params from the form and saves instance in table, then redirects to that new article's show page.
     #binding.pry
     @article = Article.create(title: params[:title], content: params[:content])
     #binding.pry
     redirect to "/articles/#{@article.id}"
   end
 
-  get '/articles/:id/edit' do
+  get '/articles/:id/edit' do 
     @article = Article.find(params[:id])
     erb :edit
   end
