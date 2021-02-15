37fb59937a9325575d2d768e85dc94c9e9285c51
Christerle Nounoute <christerlesn@gmail.com>
2021-02-15 16:55:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,11 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to '/articles/#{@article.id}'
+  end
+
   get '/articles/:id' do
     @article = Article.find(params[:id])
     erb :show
