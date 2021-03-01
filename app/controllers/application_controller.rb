7bf782db27127058a15ab5c32414f5e7951e51ff
Keith Filkins <kafilkins@me.com>
2021-02-28 19:47:55 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,21 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    @articles = Article.create(params[:title], params[:content])
+    erb :new
+  end
+
+  get '/articles' do 
+    @articles = Article.article_title
+    erb :index 
+  end
+
+  get '/articles/:id' do
+  @article = Article.find_by(params[:id])
+
+  erb :show 
+end
+
 end
