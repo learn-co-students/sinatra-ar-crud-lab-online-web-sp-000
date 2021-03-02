653ac755aca6591338cf51ec36c83adff434f271
McKayla Lankau <lankaukk@yahoo.com>
2021-03-02 00:19:57 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,45 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all 
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params) 
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    attrs = params[:article]
+    @article.update(attrs)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    end
+
 end
