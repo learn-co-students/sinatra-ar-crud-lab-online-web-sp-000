d59b4bb71318da7a44499477a5f86fdacf636c21
remote-torrent-6747 <abbyc@sky.com>
2021-04-17 10:21:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    title = params[:title]
+    content = params[:content]
+    new_article = Article.create(title: title, content: content)
+    redirect "/articles/#{new_article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    current_article = Article.find_by(id: params[:id])
+    title = params[:title]
+    content = params[:content]
+    current_article.update(title: title, content: content)
+    redirect "/articles/#{current_article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+  end
+
 end
