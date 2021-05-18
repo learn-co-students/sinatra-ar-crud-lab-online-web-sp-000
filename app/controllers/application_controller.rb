4b5be6acf4d6935a6414c6179b151d7530f0298b
Jung Lee <lee.jung.s95@gmail.com>
2021-05-18 12:47:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+      redirect to "/articles"
   end
+
+#read
+  get "/articles" do
+      @articles = Article.all
+      erb :index
+  end
+
+  get "/articles/:id" do
+      @article = Article.find(params[:id])
+      erb :show
+  end
+#new
+    get "/articles/new" do
+        @article = Article.find(params[:id])
+        erb :new
+    end
+#create
+    post "/articles" do
+        @article = Article.new(params)
+        redirect to "articles/#{@article.id}"
+    end
 end
